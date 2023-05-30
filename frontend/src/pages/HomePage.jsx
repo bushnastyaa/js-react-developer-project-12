ca61e49c9aa92b0b96638084d8e88f3137579dac
Anastasia Bushmakina <bushnastyaa@mail.ru>
2023-05-30 20:21:40 +0300

    Add fetch data

---
+++ frontend/src/pages/HomePage.jsx
@@ -0,0 +1,30 @@
+import React, { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { Container, Row } from 'react-bootstrap';
+
+import { actions as channelsActions } from '../slices/channelsSlice.js';
+import { actions as messagesActions } from '../slices/messagesSlice.js';
+import fetchData from '../routes/fetchData';
+
+function HomePage() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    fetchData()
+      .then((data) => {
+        const { channels, currentChannelId, messages } = data;
+
+        dispatch(messagesActions.addMessages(messages));
+        dispatch(channelsActions.addChannels(channels));
+        dispatch(channelsActions.setCurrentChannel(currentChannelId));
+      });
+  });
+
+  return (
+    <Container className="h-100 my-4 overflow-hidden rounded shadow">
+      <Row className="h-100 bg-white flex-md-row" />
+    </Container>
+  );
+};
+
+export default HomePage;
---
+++ frontend/src/routes/fetchData.js
@@ -0,0 +1,18 @@
+import axios from 'axios';
+import routes from './routes.js';
+
+const getAuthHeader = () => {
+  const user = JSON.parse(localStorage.getItem('userInfo'));
+  if (user && user.token) {
+    return { Authorization: `Bearer ${user.token}` };
+  }
+
+  return {};
+};
+
+const fetchData = async () => {
+  const { data } = await axios.get(routes.data(), { headers: getAuthHeader() });
+  return data;
+};
+
+export default fetchData;
