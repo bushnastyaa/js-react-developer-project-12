f89a4b07539a3c244bea39c16b0b1b0e1ead3233
Anastasia Bushmakina <bushnastyaa@mail.ru>
2023-05-31 18:42:23 +0300

    Add extraReducers

--- frontend/src/slices/channelsSlice.js
+++ frontend/src/slices/channelsSlice.js
@@ -2,7 +2,8 @@ import { createSlice, createEntityAdapter } from '@reduxjs/toolkit';
 /* eslint  no-param-reassign: 0 */
 
 const channelsAdapter = createEntityAdapter();
-const initialState = channelsAdapter.getInitialState({ currentChannelId: null });
+const initialState = channelsAdapter
+  .getInitialState({ currentChannelId: null, defaultChannelId: 1 });
 
 const channelsSlice = createSlice({
   name: 'channels',
@@ -13,13 +14,16 @@ const channelsSlice = createSlice({
     renameChannel: channelsAdapter.updateOne,
     removeChannel: channelsAdapter.removeOne,
     setCurrentChannel: (state, action) => {
-      if (action.payload === state.currentChannelId) {
-        state.currentChannelId = action.payload;
-      }
+      state.currentChannelId = action.payload;
     },
+    setChannelId: ((state, action) => {
+      if (action.payload === state.currentChannelId) {
+        state.currentChannelId = state.defaultChannelId;
+      }
+    }),
   },
 });
-  
+
 export const { actions } = channelsSlice;
 export const selectors = channelsAdapter.getSelectors((state) => state.channels);
 export default channelsSlice.reducer;
--- frontend/src/slices/messagesSlice.js
+++ frontend/src/slices/messagesSlice.js
@@ -1,4 +1,5 @@
 import { createSlice, createEntityAdapter } from '@reduxjs/toolkit';
+import { actions as channelsActions } from './channelsSlice.js';
 
 const messagesAdapter = createEntityAdapter();
 const initialState = messagesAdapter.getInitialState();
@@ -9,6 +10,16 @@ const messagesSlice = createSlice({
   reducers: {
     addMessage: messagesAdapter.addOne,
     addMessages: messagesAdapter.addMany,
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(channelsActions.removeChannel, (state, action) => {
+        const messages = Object.values(state.entities)
+          .filter(({ channelId }) => channelId === action.payload)
+          .map((message) => message.id);
+
+        messagesAdapter.removeMany(state, messages);
+      });
   },
 });
 
