c757bc82c9a95a527e1a2a41f74616094741226c
Anastasia Bushmakina <bushnastyaa@mail.ru>
2023-06-18 18:33:03 +0300

    Fix routes

--- frontend/src/routes/routes.js
+++ frontend/src/routes/routes.js
@@ -1,7 +1,7 @@
 const routes = {
-  loginPath: () => 'api/v1/login',
-  signupPath: () => 'api/v1/signup',
-  data: () => 'api/v1/data',
+  loginPath: () => '/api/v1/login',
+  signupPath: () => '/api/v1/signup',
+  data: () => '/api/v1/data',
 };
 
 export default routes;
