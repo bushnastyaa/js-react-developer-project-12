eda7d93a77fcdcf66dcd377f2393ba1affd1c2b9
Anastasia Bushmakina <bushnastyaa@mail.ru>
2023-06-19 17:29:49 +0300

    Add image src

--- frontend/src/pages/pageNotFound/PageNotFound.jsx
+++ frontend/src/pages/pageNotFound/PageNotFound.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Container } from 'react-bootstrap';
 import { useTranslation } from 'react-i18next';
+import notfoundImg from './notfoundImg.svg';
 
 const PageNotFound = () => {
   const { t } = useTranslation();
@@ -8,7 +9,7 @@ const PageNotFound = () => {
   return (
     <Container fluid className="h-100">
       <div className="text-center">
-        <img alt="Страница не найдена" style={{ width: 400 }} className="img-fluid h-25" src="./notfoundImg.svg" />
+        <img alt="Страница не найдена" style={{ width: 400 }} className="img-fluid h-25" src={notfoundImg} />
         <h1 className="h4 text-muted">{t('notFound.header')}</h1>
         <p className="text-muted">
           {t('notFound.message')}
